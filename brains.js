af606939e8dc2e8b90fd080c799df69717e233a0
celia22 <74301094+celia22@users.noreply.github.com>
2021-03-13 13:45:25 +0100

    Add files via upload

---
+++ brains.js
@@ -0,0 +1,50 @@
+class interactionObjects {
+    constructor() {
+      this.x = x;
+      this.y = y; 
+      this.size = size;
+      this.points = 0; 
+      this.images = images;
+/*
+      const images = new Image();
+      images.addEventListener('load', () => {
+      this.images = images;
+      this.draw();
+      }); */
+          
+    }
+    draw() {
+        ctx.drawImage(this.img, this.x, this.y, this.size, this.size);
+      }
+}
+
+const brainImg = new Image();
+brainImg.addEventListener('load', () => {
+this.brainImg = brainImg;
+this.draw();
+}); 
+
+brainImg.src = "./images/zombies/brain.png";
+
+ 
+const Brains = new interactionObjects (50, 100, 30, 10, brainImg) 
+console.log(Brains)
+
+/*
+
+// GREEN HOLES
+var Stick = function(x, y) { 
+    this.x = x;
+    this.y = y;
+};
+
+Stick.prototype.draw = function() {
+    fill(16, 227, 37);
+    rectMode(CENTER);
+    ellipse(this.x, this.y, 80, 80);
+};
+
+var sticks = [];
+for (var i = 0; i < 40; i++) {  
+    sticks.push(new Stick(i * 120, random(30, 300)));
+}*/
